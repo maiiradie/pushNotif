1b096e7bea20061fbba1cd2c8391071884d9491e
RyanAquino <ryan.aquino7110@gmail.com>
2018-04-08 21:48:47 +0800

    added logout functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { NotifProvider } from '../../providers/notif/notif';
+import { AuthProvider} from '../../providers/auth/auth';
 import firebase from 'firebase';
 import { FCM } from '@ionic-native/fcm';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -11,7 +13,7 @@ import { FCM } from '@ionic-native/fcm';
 })
 
 export class HomePage {
-  constructor(private fcm:FCM,private notifProvider:NotifProvider,public navCtrl: NavController, public afdb: AngularFireDatabase) {
+  constructor(private authProvider:AuthProvider,private fcm:FCM,private notifProvider:NotifProvider,public navCtrl: NavController, public afdb: AngularFireDatabase) {
   }
 
   token;
@@ -20,31 +22,38 @@ export class HomePage {
 
   ionViewDidLoad() {
 
-    this.fcm.getToken().then(token => {
-        this.afdb.object('tokens').set({
-          token:token
-        }).then( () => {
-          this.token = token;
-        });
-    });
+    // this.fcm.getToken().then(token => {
+    //     this.afdb.object('tokens').set({
+    //       token:token
+    //     }).then( () => {
+    //       this.token = token;
+    //     });
+    // });
 
-    this.fcm.onNotification().subscribe(data => {
-      if (data.wasTapped) {
-        alert("Received in background" + data);
-      } else {
-        alert("Received in foreground" + data);
-      };
-    });
+    // this.fcm.onNotification().subscribe(data => {
+    //   if (data.wasTapped) {
+    //     alert("Received in background" + data);
+    //   } else {
+    //     alert("Received in foreground" + data);
+    //   };
+    // });
 
-    this.fcm.onTokenRefresh().subscribe(token => {
-      this.afdb.object('tokens').update({
-        token: token
-      });
-    });
+    // this.fcm.onTokenRefresh().subscribe(token => {
+    //   this.afdb.object('tokens').update({
+    //     token: token
+    //   });
+    // });
   }
 
   sendNotif(){
     this.notifProvider.sendRequest(this.token);
+  }
+
+  onLogout(){
+    this.authProvider.logoutUser()
+    .then( () => {
+      this.navCtrl.setRoot(LoginPage);
+    });
   }
 
   // ionViewDidLeave() {
